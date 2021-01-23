3286da4e529fbe0e227f2d11b174c11c459e4fcf
José Alberto Trejo Serrano <joseatrejos@gmail.com>
2021-01-22 22:33:56 -0700

    Added comments

--- fases-eventloop.js
+++ fases-eventloop.js
@@ -1,26 +1,26 @@
 const fs = require('fs');
 
-// 4th Process: Close?
+// 2nd Process: Check.
 setImmediate( () => console.log(1) );
 
-// 3rd Process: Close?
+// Microtask queue, which is executed between processes (in this case between Poll and Check). rejects and resolves go after nextTick.
 Promise.resolve().then( () => console.log(2) );
 
-// 2nd Process: Check?
+// Microtask queue, which is executed between processes (in this case between Poll and Check). nextTick executes before resolves and rejects.
 process.nextTick( () => console.log(3) );
 
-// 5th Process: Pending?: 
+// Once the eventLoop is finished, this is done in the Poll process. | *NOTE* the processes 3-5 (Close, Timers, and Pending) weren't used.
 fs.readFile("hola.txt", () => {
-    // Inside this, the loop applies the same; 1st goes the Poll
+    // Inside this, the loop applies the same; 1st goes the Poll.
     console.log(4);
 
-    // 4th go the timers
+    // Finally, the timers are executed (Process number 4).
     setTimeout( () => console.log(5) );
     setTimeout( () => console.log(6) );
 
-    // 2nd goes the Check
+    // 2nd goes the Microtask queue between processes
     process.nextTick( () => console.log(7) );
 });
 
-// 1st Process: Poll
+// 1st Process: Poll.
 console.log(8);
